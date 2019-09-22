d626b96086f733d050eb0ad78202caf10b5d3348
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-09-22 14:26:24 -0300

    add color by status

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Card.css';
 
+import moment from 'moment';
+
 export default function Card({ item }) {
+	const [lastUpdated, setLastUpdated] = useState(
+		moment(item.updatedAt).fromNow()
+	);
+
+	let borderTop = null;
+
+	if (item.status !== '' && item.status == 'ok') {
+		borderTop = '40px solid rgb(145,221,213)';
+	} else if (item.status !== '' && item.status == 'nok') {
+		borderTop = '40px solid rgb(241,173,183)';
+	}
+
+	useEffect(() => {
+		setInterval(() => {
+			setLastUpdated(moment(item.updatedAt).fromNow());
+		}, 60000);
+	}, []);
+
 	return (
-		<div className="card-container">
+		<div className="card-container" style={{ borderTop }}>
 			<header />
 			<p>{item.name}</p>
-			<label>{item.stage}</label>
+			<label>{lastUpdated}</label>
 		</div>
 	);
 }
